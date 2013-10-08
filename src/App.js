750ce94848ce871cf2af0748b731127706685639
N M <nmusaelian@rallydev.com>
2013-10-08 13:59:55 -0600

    added chart reload with new data

--- src/App.js
+++ src/App.js
@@ -3,36 +3,125 @@ Ext.define('CustomApp', {
     componentCls: 'app',
 
     launch: function() {
+        if (this.down("#ch")) {
+        console.log("chart exists");
+       }
+       else{
+        console.log("chart does not exists");
+       }
         var that = this;
-        var that = this;
-        var store = Ext.create('Ext.data.JsonStore', {
+        this._store = Ext.create('Ext.data.JsonStore', {
+            autoLoad: true,
             fields: ['x', 'Keep Going','Swerve'],
             data: [
                 { 'x': 'Keep Going', 'Keep Going' : '-10,-10', 'Swerve' : '2,-2' },
                 { 'x': 'Swerve','Keep Going' : '-2,2', 'Swerve' : '0,0' }
-            ]
+            ] 
         });
        this._series = [];
-       this._play();
-        
-        var myGrid = Ext.create('Ext.grid.Panel', {
-            title: 'matrix',
-            store: store,
-            sortableColumns: false,
-            enableColumnMove: false,
-            enableColumnHide: false,
-            columns: [
-                { text: 'x',  dataIndex: 'x' },
-                { text: 'Keep Going', dataIndex: 'Keep Going'},
-                { text: 'Swerve', dataIndex: 'Swerve' }
-            ],
+       var c = Ext.create('Ext.Container', {
+            items: [
+                {
+                    xtype  : 'rallybutton',
+                    text      : 'reload with new data',
+                    id: 'b1',
+                    handler: function() {
+                        that._updateChart();
+                    }
+                },
+                {
+                    xtype : 'grid',
+                    title: 'matrix',
+                    id: 'g',
+                    store: this._store,
+                    enableColumnMove: false,
+                    enableColumnHide: false,
+                    columns: [
+                        { text: 'x',  dataIndex: 'x' },
+                        { text: 'Keep Going', dataIndex: 'Keep Going'},
+                        { text: 'Swerve', dataIndex: 'Swerve' }
+                    ],
             height: 100,
             width: 307
-        });
-        this.add(myGrid);
+                }
+            ]
+    });
+       this.add(c); 
+       this._play();
+
+    },
+    _play:function(){
+        this._data = [[],[]];
+        var rounds = 10;
+        var players = 2;
+        var playersArr = ['Player 1', 'Player 2'];
+        this._moves = [[],[]];
+        var notRandom = ['keep going','swerve','swerve','swerve','swerve'];
+        for (var p=0;p<players;p++) {
+            for (var i = 0; i<rounds; i++) {
+                var x = Math.floor(Math.random()*notRandom.length);
+                this._moves[p].push(notRandom[x]);
+                if (p===players-1) { 
+                   if ((this._moves[p][i] == "keep going")&&(this._moves[p-1][i] === "keep going")) {
+                    this._data[p][i]=[i, -10,this._moves[p][i]];
+                    this._data[p-1][i]=[i, -10,this._moves[p-1][i]];
+                    
+                   }
+                   else if ((this._moves[p][i] == "swerve")&&(this._moves[p-1][i] === "keep going")) {
+                    this._data[p][i]=[i, -2,this._moves[p][i]];
+                    this._data[p-1][i]=[i, 2,this._moves[p-1][i]];
+                    
+                   }
+                   else if ((this._moves[p][i] == "keep going")&&(this._moves[p-1][i] === "swerve")) {
+                    this._data[p][i]=[i, 2,this._moves[p][i]];
+                    this._data[p-1][i]=[i, -2,this._moves[p-1][i]];
+                   }
+                   else if ((this._moves[p][i] == "swerve")&&(this._moves[p-1][i] === "swerve")) {
+                    this._data[p][i]=[i, 0,this._moves[p][i]];
+                    this._data[p-1][i]=[i, 0,this._moves[p-1][i]];
+                   }
+                }    
+            }
+        }
+        this._series.push({
+		    	name: playersArr[0],
+		    	data: this._data[0],
+                        color: 'rgba(223, 83, 83, .5)'
+				})
+        this._series.push({
+		    	name: playersArr[1],
+		    	data: this._data[1],
+                        color: 'rgba(119, 152, 191, .5)'
+				})
+        
+        console.log("series", this._series);
+        this._makeChart();
+        
+    },
+    
+    
+    _updateChart:function(){
+        //Ext.getCmp("#ch").unmask();
+        //this.down("#ch").unmask();
+       if (this.down("#ch")) {
+        console.log("chart exists. Removing chart...");
+       }
+       this.down("#ch").removeAll();
+       this._series.length = 0;
+       this._data.length = 0;
+       this._play();
+       
+    },
+    
+    _makeChart:function(){
         this.add(
         {
             xtype: 'rallychart',
+            viewConfig: {
+                loadMask: false
+            },
+            id: 'ch',
+            //itemId: 'myChart',
             height: 400,
             chartConfig: {
                 chart:{
@@ -71,7 +160,7 @@ Ext.define('CustomApp', {
                 },
                 tooltip: {
                         formatter: function() {
-                            var info = this.series.name + '<br> ' + ' ' + 'round: <b>'+ this.x +'</b> payoff <b>'+ this.y +'</b>' + this.point.config[2];
+                            var info = this.series.name  + '<b> ' + this.point.config[2]  + '</b>' + '<br> ' + ' ' + 'round: <b>'+ this.x +'</b> payoff <b>'+ this.y +'</b>';
                             return info;
                         }
         },
@@ -92,12 +181,7 @@ Ext.define('CustomApp', {
                                     enabled: false
                                 }
                             }
-                        },
-                        /*
-                        tooltip: {
-                            headerFormat: '<b>{series.name}</b><br>',
-                            pointFormat: 'round: {point.x} , payoff: {point.y}'           
-                        }*/
+                        }
                     }
                 },
             },
@@ -105,57 +189,10 @@ Ext.define('CustomApp', {
             chartData: {
                 series: this._series
             }
+          
         });
-    },
-    _play:function(){
-        //this._data = [[0,0,''],[0,0,'']];
-        this._data = [[0,0],[0,0]];
-        var rounds = 10;
-        var players = 2;
-        var playersArr = ['Player 1', 'Player 2'];
-        this._moves = [[],[]];
-        var notRandom = ['keep going','swerve','swerve','swerve','swerve'];
-        for (var p=0;p<players;p++) {
-            for (var i = 0; i<rounds; i++) {
-                var x = Math.floor(Math.random()*notRandom.length);
-                this._moves[p].push(notRandom[x]);
-                if (p===players-1) { 
-                   if ((this._moves[p][i] == "keep going")&&(this._moves[p-1][i] === "keep going")) {
-                    this._data[p][i]=[i, -10,this._moves[p][i]];
-                    this._data[p-1][i]=[i, -10,this._moves[p-1][i]];
-                    
-                   }
-                   else if ((this._moves[p][i] == "swerve")&&(this._moves[p-1][i] === "keep going")) {
-                    this._data[p][i]=[i, -2,this._moves[p][i]];
-                    this._data[p-1][i]=[i, 2,this._moves[p-1][i]];
-                    
-                   }
-                   else if ((this._moves[p][i] == "keep going")&&(this._moves[p-1][i] === "swerve")) {
-                    this._data[p][i]=[i, 2,this._moves[p][i]];
-                    this._data[p-1][i]=[i, -2,this._moves[p-1][i]];
-                   }
-                   else if ((this._moves[p][i] == "swerve")&&(this._moves[p-1][i] === "swerve")) {
-                    this._data[p][i]=[i, 0,this._moves[p][i]];
-                    this._data[p-1][i]=[i, 0,this._moves[p-1][i]];
-                   }
-                }    
-            }
-            console.log("DATA", this._data);
-            console.log("DATA[0]", this._data[0]);
-            console.log("DATA[1]", this._data[1]);
-        }
-        this._series.push({
-		    	name: playersArr[0],
-		    	data: this._data[0],
-                        color: 'rgba(223, 83, 83, .5)'
-				})
-        this._series.push({
-		    	name: playersArr[1],
-		    	data: this._data[1],
-                        color: 'rgba(119, 152, 191, .5)'
-				})
-        
-        console.log("series..................", this._series);
-        
+        this.down('#ch')._unmask();
+         //this.down("#ch").unmask();
+        //Ext.getCmp("ch").unmask(); 
     }
 });
